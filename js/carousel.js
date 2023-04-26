c5a27d77aed6332c282907645a426811d306cf99
feine-luis <131889816+feine-luis@users.noreply.github.com>
2023-04-26 14:28:26 -0300

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,26 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 5,
+      margin: 30,
+      loop: true,
+      nav: true,
+      responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+    });
+  });
